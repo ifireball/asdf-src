85a1e0604e7fd8571b01bd3209980101a937067c
Barak Korren <bkorren@redhat.com>
2024-08-05 23:14:41 +0300

    feat: Adapt code for src

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for src.
 GH_REPO="https://github.com/ifireball/src"
 TOOL_NAME="src"
 TOOL_TEST="src --help"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if src has other means of determining installable versions.
 	list_github_tags
 }
 
@@ -41,8 +38,9 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for src
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	kernel="$(uname -s | tr '[:upper:]' '[:lower:]')"
+	arch="$(uname -m)"
+	url="$GH_REPO/releases/download/v${version}/src.$kernel.$arch"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,10 +59,10 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert src executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		test -f "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to exist."
+		chmod +x "$install_path/$tool_cmd" || fail "Failed to make $install_path/$tool_cmd executable"
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
